e731f2f7027989d57d90a82bb5cc05de5a182d22
Quynh Nguyen <seriquynh@gmail.com>
2024-12-09 13:46:40 +0700

    Add navigation.ts file

---
+++ web/navigation.ts
@@ -0,0 +1,14 @@
+export const navigation = [
+    {
+        title: 'Introduction',
+        links: [
+            { title: 'Getting started', href: '/' },
+        ],
+    },
+    {
+        title: 'Commands',
+        links: [
+            { title: 'Security setup', href: '/docs/security-setup' },
+        ],
+    },
+]
