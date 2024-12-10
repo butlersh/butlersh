091c1e7d9a09d412f7239cdd391ee3750414e896
Quynh Nguyen <seriquynh@gmail.com>
2024-12-10 09:36:58 +0700

    WIP

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
 WIP
-
-## Development
-
-Try server-cli.sh inside a Ubuntu container.
-
-```bash
-docker run -it --rm -w /usr/src/app -v .:/usr/src/app ubuntu:24.04
-```
--- installer.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-wget -qO- "https://raw.githubusercontent.com/butlersh/butlersh/main/server-cli.sh" > server-cli.sh
-
-chmod +x server-cli.sh
-
-mv server-cli.sh /usr/local/bin/butlersh
