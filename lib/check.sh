3e832a7f96780dc4ee5da7086042524c2f4049a6
Quynh Nguyen <seriquynh@gmail.com>
2024-12-17 10:27:36 +0700

    Check root privileges & supported OS

--- lib/check.sh
+++ lib/check.sh
@@ -1,36 +1,48 @@
 check_root_privileges() {
-    if [ "$USER" != 'root' ]; then
-        echo 'butlersh.ERROR: Please run this script as root.'
+    F_USER=$(whoami)
+    if [ "$F_USER" != 'root' ]; then
+        io_print_error 'Please run this command/script as root.'
         exit 1
     fi
 }
 
 check_supported_os() {
-  # TODO: If lsb_release does not exit, use /etc/os-release instead.
-  OS_DISTRIB_NAME=${OS_DISTRIB_NAME:-$(lsb_release -is)}
-  OS_RELEASE_NAME=${OS_RELEASE_NAME:-$(lsb_release -cs)}
+    OS_DISTRO_NAME='unsupported'
+    OS_RELEASE_NAME='unsupported'
 
-  case "${OS_DISTRIB_NAME}" in
-    "Ubuntu" | "ubuntu")
-      DISTRIB_NAME="ubuntu"
-      case "${OS_RELEASE_NAME}" in
-        "noble" | "jammy" | "focal")
-          RELEASE_NAME="${OS_RELEASE_NAME}"
+    # If lsb_release exists, we'll it to determine OS_DISTRO_NAME and OS_RELEASE_NAME.
+    if [ -f /usr/bin/lsb_release ]; then
+        OS_DISTRO_NAME=${OS_DISTRO_NAME:-$(lsb_release -is)}
+        OS_RELEASE_NAME=${OS_RELEASE_NAME:-$(lsb_release -cs)}
+    # If /etc/os-release exists, we'll it to determine OS_DISTRO_NAME and OS_RELEASE_NAME.
+    elif [ -f /etc/os-release ]; then
+        if [ -n "$(cat /etc/os-release | grep ubuntu)" ]; then
+            OS_DISTRO_NAME='ubuntu'
+            OS_RELEASE_NAME=$(cat /etc/os-release | grep VERSION_CODENAME | cut -d'=' -f2)
+        fi
+    fi
+
+    case "${OS_DISTRO_NAME}" in
+        "Ubuntu" | "ubuntu")
+            DISTRO_NAME="ubuntu"
+            case "${OS_RELEASE_NAME}" in
+                "noble" | "jammy" | "focal")
+                    RELEASE_NAME="${OS_RELEASE_NAME}"
+                ;;
+                *)
+                    RELEASE_NAME="unsupported"
+                ;;
+            esac
         ;;
         *)
-          RELEASE_NAME="unsupported"
+            DISTRO_NAME="unsupported"
         ;;
-      esac
-    ;;
-    *)
-      DISTRIB_NAME="unsupported"
-    ;;
-  esac
+    esac
 
-  if [[ "${DISTRIB_NAME}" == "unsupported" || "${RELEASE_NAME}" == "unsupported" ]]; then
-    echo "This Linux distribution isn't supported yet."
-    echo "If you'd like it to be, let us know!"
-    echo "👉🏻 https://github.com/butlersh/butlersh/issues"
-    exit 1
-  fi
+    if [[ "${DISTRO_NAME}" == "unsupported" || "${RELEASE_NAME}" == "unsupported" ]]; then
+        io_comment "This Linux distribution isn't supported yet."
+        io_comment "If you'd like it to be, let us know!"
+        io_comment "👉🏻 https://github.com/butlersh/butlersh/issues"
+        exit 1
+    fi
 }
