b945c18e4ebb8d782dff4c55207d01e7eeed82c3
Quynh Nguyen <seriquynh@gmail.com>
2024-12-17 11:55:31 +0700

    help

--- command/help.sh
+++ command/help.sh
@@ -13,23 +13,29 @@ run_help_command() {
         "list")
             help_list_command
         ;;
+        "mysql:purge")
+            help_mysql_purge_command
+        ;;
         "mysql:setup")
             help_mysql_setup_command
+        ;;
+        "nginx:purge")
+            help_nginx_purge_command
         ;;
         "nginx:setup")
             help_nginx_setup_command
         ;;
-        "php:setup")
-            help_php_setup_command
-        ;;
         "php:purge")
             help_php_purge_command
+        ;;
+        "php:setup")
+            help_php_setup_command
         ;;
         "security:setup")
             help_security_setup_command
         ;;
         *)
-            io_print_error "The <comment>$RUNNING_COMMAND</comment> command was not found." exit 1
+            io_print_error "The <comment>$1</comment> command was not found." && exit 1
         ;;
     esac
 }
